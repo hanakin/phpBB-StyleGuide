01cab5220539152eaca544bf0311c4311029d8c2
hanakin <midaym@gmail.com>
2014-08-16 19:00:43 +0200

    test ie 4

--- assets/js/application.js
+++ assets/js/application.js
@@ -5,43 +5,44 @@
         $(this).closest('.form-control-group').removeClass('focus');
     });
 
-    $(function() {
+$(function() {
 
-        // Set active nav link state
-        $('.nav li a:first').click().addClass('active');
-        $('.nav a').on('click', function() {
-            $(this).parent().parent().find('a.active').removeClass('active');
-            $(this).addClass('active');
-        });
-
-        $('.features-nav-top').on('click', function() {
-            var navUL = $(this).parent().parent();
-            navUL.find('a.active').removeClass('active');
-            navUL.find('li a:first').addClass('active');
-        });
-
-        // Fix navigation on splash page when scrolled past
-        var navCurrent = $('.features-nav').offset().top;
-        var fixedNav = function(){
-            var scrollTop = $(window).scrollTop();
-
-            if ( scrollTop > navCurrent) {
-                $('.features-nav').addClass('features-nav-fixed');
-                $('.features-nav .nav').addClass('nav-top-show');
-                $('.features-nav-top').addClass('features-nav-top-show');
-            } else {
-                $('.features-nav').removeClass('features-nav-fixed');
-                $('.features-nav .nav').removeClass('nav-top-show');
-                $('.features-nav-top').removeClass('features-nav-top-show');
-            }
-        };
-
-        fixedNav();
-
-        $(window).scroll(function() {
-            fixedNav();
-        });
+    // Set active nav link state
+    $('.nav a:first').click().addClass('active');
+    $('.nav a').on('click', function() {
+        $(this).parent().find('a.active').removeClass('active');
+        $(this).addClass('active');
     });
+
+    $('.features-nav-top').on('click', function() {
+        var nav = $(this).parent();
+        nav.find('a.active').removeClass('active');
+        nav.find('a:first').addClass('active');
+    });
+
+    // Fix navigation on splash page when scrolled past
+    var navCurrent = $('.features-nav').offset().top;
+    var fixedNav = function(){
+        var scrollTop = $(window).scrollTop();
+
+        if ( scrollTop > navCurrent) {
+            $('.features-nav').addClass('features-nav-fixed');
+            $('.features-nav .nav').addClass('nav-top-show');
+            $('.features-nav-top').addClass('features-nav-top-show');
+        } else {
+            $('.features-nav').removeClass('features-nav-fixed');
+            $('.features-nav .nav').removeClass('nav-top-show');
+            $('.features-nav-top').removeClass('features-nav-top-show');
+        }
+    };
+
+    fixedNav();
+
+    $(window).scroll(function() {
+        fixedNav();
+    });
+});
+
 
 
 
