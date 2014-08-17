8e6f0bb0598dcc9d815545df94d2763743ff5ba4
hanakin <midaym@gmail.com>
2014-08-17 14:45:11 +0200

    Fix ie nav

--- assets/js/application.js
+++ assets/js/application.js
@@ -6,42 +6,144 @@
     });
 
 $(function() {
+    //Cache the page
+    var $page = $('html, body');
 
-    // Set active nav link state
-    $('.nav a:first').click().addClass('active');
-    $('.nav a').on('click', function() {
-        $(this).parent().find('a.active').removeClass('active');
-        $(this).addClass('active');
-    });
+    // Set the speed of the assisted page scroll
+    var pageScrollSpeed = 500;
 
-    $('.features-nav-top').on('click', function() {
-        var nav = $(this).parent();
-        nav.find('a.active').removeClass('active');
-        nav.find('a:first').addClass('active');
-    });
+    // -------------------------------------------------------------------------
+    // SETUP - NAVIGATION
+    // -------------------------------------------------------------------------
+    // Cache Navigation in jQuery objects
+    var $navContainer = $('.features-nav');
 
-    // Fix navigation on splash page when scrolled past
-    var navCurrent = $('.features-nav').offset().top;
-    var fixedNav = function(){
-        var scrollTop = $(window).scrollTop();
+    // All nav items
+    var $navLinks = $('.nav-link');
+    // Specific nav item, e.g., navItem.$home
 
-        if ( scrollTop > navCurrent) {
-            $('.features-nav').addClass('features-nav-fixed');
-            $('.features-nav .nav').addClass('nav-top-show');
-            $('.features-nav-top').addClass('features-nav-top-show');
-        } else {
-            $('.features-nav').removeClass('features-nav-fixed');
-            $('.features-nav .nav').removeClass('nav-top-show');
-            $('.features-nav-top').removeClass('features-nav-top-show');
-        }
+    var navLink = {
+        "$admin": $navLinks.filter('.admin'),
+        "$user":  $navLinks.filter('.user'),
+        "$dev":   $navLinks.filter('.dev'),
+        "$des":   $navLinks.filter('.des'),
+        "$home":  $navLinks.filter('.top')
     };
 
-    fixedNav();
 
-    $(window).scroll(function() {
-        fixedNav();
+    // -------------------------------------------------------------------------
+    // SETUP - PAGE CONTENT
+    // -------------------------------------------------------------------------
+
+    // Specific section, e.g., section.$home
+    var section = {
+        "$admin": $('#feture-admin'),
+        "$user":  $('#feture-user'),
+        "$dev":   $('#feture-dev'),
+        "$des":   $('#feture-des'),
+        "$home":  $('#phpbb')
+    };
+
+
+    // -------------------------------------------------------------------------
+    // SETUP - HELPER FUNCTIONS
+    // -------------------------------------------------------------------------
+
+    var pageScrollTo = function (target) {
+        target = target > 0 ? target : target.offset().top;
+        $page.animate({ 'scrollTop': target }, pageScrollSpeed);
+    };
+
+    // -------------------------------------------------------------------------
+    // SETUP - EVENT BINDINGS
+    // -------------------------------------------------------------------------
+
+    // CLICK EVENTS ------------------------------------------------------------
+
+    $navLinks.on('click', function (event) {
+        var $this = $(this);
+        var destination = $this.attr('href');
+
+        event.preventDefault();
+
+        $navLinks.removeClass('active');
+        if(destination == '#phpbb' ) {
+            navLink.$admin.addClass('active');
+        } else {
+            $this.addClass('active');
+        }
+        pageScrollTo($(destination));
     });
+
+    // // END CLICK EVENTS -----------------------------------------------------
+
+    // // END WAYPOINT BINDINGS ------------------------------------------------
+
+    section.$admin.waypoint(function(){
+        $navLinks.removeClass('active');
+        navLink.$admin.addClass('active');
+    });
+
+    section.$user.waypoint(function(){
+        $navLinks.removeClass('active');
+        navLink.$user.addClass('active');
+    });
+
+    section.$dev.waypoint(function(){
+        $navLinks.removeClass('active');
+        navLink.$dev.addClass('active');
+    });
+
+    section.$des.waypoint(function(){
+        $navLinks.removeClass('active');
+        navLink.$des.addClass('active');
+    });
+
+    section.$home.waypoint(function(){
+        $navLinks.removeClass('active');
+        navLink.$admin.addClass('active');
+    });
+
+    $navContainer.waypoint('sticky');
 });
+
+
+
+    // var nav         = $('.nav');
+    // var navFirst    = $('.nav a:first');
+    // var navLinks    = $('.nav a');
+    // var navTop      = $('.features-nav-top');
+    // var navOuter    = $('.features-nav');
+    // var navPosition = navOuter.offset().top;
+
+    // Set active nav link state
+    // navLinks.on('click', function() {
+    //     navItems.removeClass('active');
+    //     $(this).addClass('active');
+    // });
+
+    // navItem.home.on('click', function() {
+    //     navItems.removeClass('active');
+    //     $(this).addClass('active');
+    // });
+
+    // Fix navigation on splash page when scrolled past
+//     var fixedNav = function(){
+//         var scrollTop = $(window).scrollTop();
+//         if ( scrollTop > navPosition) {
+//             navOuter.addClass('features-nav-fixed');
+//         } else {
+//             navOuter.removeClass('features-nav-fixed');
+//         }
+//     };
+
+//     navItem.admin.click().addClass('active');
+//     fixedNav();
+
+//     $(window).on('scroll', function() {
+//         fixedNav();
+//     });
+// });
 
 
     // $(document).ready(function ( {
