d2cf58002775a8eb81cc1eb7a5560e4a69cc6605
hanakin <midaym@gmail.com>
2014-09-07 15:32:54 +0200

    Presentation & Oranization Overhaul
    - Impliment template system for better contorl over html content (gulp-twig)
    - Refactor all code for consistency
    - Re-design index to better control versioning and modularity
    - Impliment testing base for individual component testing
    - Add navigation
    - Simplify process for adding components to allow for contribution
    * Todo make the minificiation process cleaner and more efficient

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,9 @@
 'use strict';
-var gulp = require('gulp');
-
-// package.json
-var pkg = require('./package.json');
 
 // Plugins
-var notify          = require("gulp-notify"),
-    gutil           = require('gulp-util'),
-    plumber         = require('gulp-plumber'),
-    less            = require('gulp-less'),
-    sourcemaps      = require('gulp-sourcemaps'),
-    minifyCSS       = require('gulp-minify-css'),
-    csscomb         = require('gulp-csscomb'),
-    imagemin        = require('gulp-imagemin'),
-    cache           = require('gulp-cache'),
-    swig            = require('gulp-swig'),
-    htmlPrettify    = require('gulp-html-prettify'),
-    rename          = require("gulp-rename"),
-    csslint         = require('gulp-csslint');
+var gulp            = require('gulp'),
+    es              = require('event-stream'),
+    plugins         = require('gulp-load-plugins')();
 
 // Dir Variables
 var assetsDir   = 'assets/',
@@ -28,107 +14,155 @@ var assetsDir   = 'assets/',
     imgDir      = assetsDir + 'imgs/';
 
 // Functions
-var customReporter = function(file) {
-    gutil.log(gutil.colors.cyan(file.csslint.errorCount)+' errors in '+gutil.colors.magenta(file.path));
+var customReporter = function (file) {
+    plugins.util.log(
+        plugins.util.colors.cyan(file.csslint.errorCount)
+        + ' errors in '
+        + plugins.util.colors.magenta(file.path)
+    );
 
     file.csslint.results.forEach(function(result) {
-        gutil.log(gutil.colors.red(result.error.message)+' on line '+gutil.colors.green(result.error.line));
+        plugins.util.log(
+            plugins.util.colors.red(result.error.message)
+            + ' on line '
+            + plugins.util.colors.green(result.error.line)
+        );
     });
-};
-
-var onError = function (err) {
-    gutil.log(gutil.colors.red(err));
 };
 
 // Tasks
 gulp.task('html:index', function () {
-    return gulp.src(htmlDir + 'index.swig')
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(swig())
-        .pipe(htmlPrettify({indent_char: ' ', indent_size: 4}))
-        .pipe(gulp.dest('./'))
-        .pipe(notify({
-            "message": "HTML: index.html Generated!",
-            "onLast": true
-        }));
+    var htmlStream = plugins.twig({
+        strict_variables: false,
+        data: require('./views/global.json')
+    });
+
+    htmlStream.on('error', function (err) {
+        plugins.util.log(plugins.util.colors.red.bold('INDEX - TWIG error:'), err.message);
+    });
+
+    var PrettifyStream = plugins.htmlPrettify({indent_char: ' ', indent_size: 4});
+
+    PrettifyStream.on('error', function (err) {
+        plugins.util.log(plugins.util.colors.red.bold('Prettify error:'), err.message);
+    });
+
+    var stream = gulp.src(htmlDir + 'index.twig');
+    stream = stream
+                .pipe(htmlStream)
+                .pipe(PrettifyStream)
+                .pipe(plugins.notify({
+                    'message': 'HTML: index.html Generated!',
+                    'onLast': true
+                }));
+
+    stream.pipe(gulp.dest('./'));
 });
 
 gulp.task('html:test', function () {
-    return gulp.src(htmlDir + 'tests/**/*.swig')
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(swig())
-        .pipe(htmlPrettify({indent_char: ' ', indent_size: 4}))
-        .pipe(gulp.dest('./tests'))
-        .pipe(notify({
-            "message": "HTML: tests Generated!",
-            "onLast": true
-        }));
+    var htmlStream = plugins.twig({
+        strict_variables: false,
+        data: require('./views/global.json')
+    });
+
+    htmlStream.on('error', function (err) {
+        plugins.util.log(plugins.util.colors.red.bold('TEST - TWIG error:'), err.message);
+    });
+
+    var PrettifyStream = plugins.htmlPrettify({indent_char: ' ', indent_size: 4});
+
+    PrettifyStream.on('error', function (err) {
+        plugins.util.log(plugins.util.colors.red.bold('Prettify error:'), err.message);
+    });
+
+    var stream = gulp.src(htmlDir + 'tests/**/*.twig');
+
+    stream = stream
+                .pipe(htmlStream)
+                .pipe(PrettifyStream)
+                .pipe(plugins.notify({
+                    'message': 'HTML: tests Generated!',
+                    'onLast': true
+                }));
+
+    stream.pipe(gulp.dest('./tests'));
 });
 
 gulp.task('css:build', function () {
-    return gulp.src([lessDir + 'style.less', lessDir + 'core.less', lessDir +'theme.less'])
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(less()).pipe(notify({"message": "LESS: Rendered!", "onLast": true}))
-        .pipe(csscomb('csscomb')).pipe(notify({"message": "CSS: Sorted!", "onLast": true}))
-        .pipe(sourcemaps.write('./')).pipe(notify({"message": "CSS: Maped!", "onLast": true}))
-        .pipe(gulp.dest(cssDir));
+    var stream = gulp.src([
+                            lessDir + 'style.less',
+                            lessDir + 'core.less',
+                            lessDir + 'theme.less'
+                        ]);
+
+    stream = stream
+                .pipe(plugins.sourcemaps.init())
+                .pipe(plugins.less()).pipe(plugins.notify({'message': 'LESS: Rendered!','onLast': true}))
+                .pipe(plugins.csscomb('csscomb.json')).pipe(plugins.notify({'message': 'CSS: Sorted!','onLast': true}))
+                .pipe(plugins.sourcemaps.write('./'))
+                .pipe(plugins.notify({'message': 'CSS: Maped!','onLast': true}));
+
+    stream.pipe(gulp.dest('./dist/' + cssDir));
 });
 
 gulp.task('css:minify', function () {
-    return gulp.src([lessDir + 'style.css', cssDir + 'core.css', cssDir +'theme.css'])
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(minifyCSS({
-            keepBreaks: false,
-            processImport: true,
-            noAdvanced: false
-        }))
-        .pipe(rename({dirname: "min", suffix: ".min"}))
-        .pipe(gulp.dest(cssminDir))
-        .pipe(notify({"message": "CSS: Minified!", "onLast": true}));
-});
+    var stream = gulp.src([
+                            './dist/' + cssDir + 'style.css',
+                            './dist/' + cssDir + 'core.css',
+                            './dist/' + cssDir + 'theme.css'
+                        ]);
 
-gulp.task('lint:core', function() {
-    gulp.src(cssDir + 'core.css')
-        .pipe(csslint('csslintrc.json'))
-        .pipe(csslint.reporter(customReporter));
-});
+    stream = stream
+                .pipe(plugins.minifyCss({
+                    keepBreaks: false,
+                    processImport: true,
+                    noAdvanced: false
+                }))
+                .pipe(plugins.rename({dirname: 'min', suffix: '.min'}))
+                .pipe(plugins.notify({'message': 'CSS: Minified!','onLast': true}));
 
-gulp.task('lint:theme', function() {
-    gulp.src(cssDir + 'theme.css')
-        .pipe(csslint('csslintrc.json'))
-        .pipe(csslint.reporter(customReporter));
+    stream.pipe(gulp.dest('./dist/' + cssminDir));
 });
 
 gulp.task('img:crush', function () {
-    return gulp.src(imgDir + '*.png')
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(cache(imagemin({
-            progressive: true,
-            interlaced: true
-        })))
-        .pipe(gulp.dest(imgDir))
-        .pipe(notify({
-            "message": "IMAGES: Crushed!",
-            "onLast": true
-        }));
+    var imgStream = plugins.imagemin({
+                        progressive: true,
+                        interlaced: true
+                    });
+
+    imgStream.on('error', function (err) {
+        plugins.util.log(plugins.util.colors.red.bold('Image error:'), err.message);
+    });
+
+    var stream = gulp.src(imgDir + '*.png');
+
+    stream = stream
+                .pipe(plugins.cache(imgStream))
+                .pipe(plugins.notify({
+                    'message': 'IMAGES: Crushed!',
+                    'onLast': true
+                }));
+
+    stream.pipe(gulp.dest('./dist/' + imgDir));
+});
+
+gulp.task('lint:core', function () {
+    gulp.src(cssDir + 'core.css')
+        .pipe(plugins.csslint('csslintrc.json'))
+        .pipe(plugins.csslint.reporter(customReporter));
+});
+
+gulp.task('lint:theme', function () {
+    gulp.src(cssDir + 'theme.css')
+        .pipe(plugins.csslint('csslintrc.json'))
+        .pipe(plugins.csslint.reporter(customReporter));
 });
 
 gulp.task('watch', function () {
     gulp.watch(htmlDir + '**/*.*', ['html:index', 'html:test']);
-    gulp.watch(lessDir + '**/*.*', ['css:build']);
-    gulp.watch(cssDir + '**/*.*', ['css:minify']);
+    gulp.watch(lessDir + '**/*.less', ['css:build']);
+    gulp.watch('./dist/' + cssDir + '/*.css', ['css:minify']);
     gulp.watch(imgDir + '**/*.*', ['img:crush']);
 });
 
-gulp.task('default', ['css:build', 'html:index','watch']);
+gulp.task('default', ['css:build', 'html:index', 'html:test', 'watch']);
